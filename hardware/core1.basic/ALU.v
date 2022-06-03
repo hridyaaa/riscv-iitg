43af470edf0c31a09f2667d48a1fe01a7cceb931
hridyaaa <106798734+hridyaaa@users.noreply.github.com>
2022-06-03 12:04:22 +0530

    Update ALU.v

--- hardware/core1.basic/ALU.v
+++ hardware/core1.basic/ALU.v
@@ -14,7 +14,7 @@ module ALU(
 	wire wire_sub_en = ( control==8'h02 ) ; 
 	wire wire_xor_en = ( control==8'h03 ) ; 
 	wire wire_or_en  = ( control==8'h04 ) ; 
-	wire wire_and_en = ( control==8'h05 ) ; 
+	wire wire_and_en = ( control==8'h05 ) ; //select lines for arithmatic operations
 
 // Arithmetic
 //---------------------------------------------------------
@@ -23,29 +23,29 @@ module ALU(
 	wire [31:0] wire_sub32 = din1 - din2 ;
 	wire [31:0] wire_xor32 = din1 ^ din2 ;
 	wire [31:0] wire_or32  = din1 | din2 ;
-	wire [31:0] wire_and32 = din1 & din2 ;
+	wire [31:0] wire_and32 = din1 & din2 ;//arithmatic operations
 	
 // Comparator
 //---------------------------------------------------------
 	wire signed [31:0] sign_din1 = din1;
-	wire signed [31:0] sign_din2 = din2;
+	wire signed [31:0] sign_din2 = din2;//for comparison purpose ,sign bit is important.therefore changed din and din2 to signed values.
 	
 	wire wire_equal   = ( din1 == din2 );
 	wire wire_nequal  = ( din1 != din2 );
 	wire wire_lesser  = ( sign_din1 <  sign_din2 );
 	wire wire_gequal  = ( sign_din1 >= sign_din2 );
 	wire wire_ulesser = ( din1 <  din2 );
-	wire wire_ugequal = ( din1 >= din2 );
+	wire wire_ugequal = ( din1 >= din2 );//comparison operations
 	
 // Shift
 //---------------------------------------------------------
 	wire [ 4:0]  operand = din2[4:0];
     wire [31:0] wire_sll = din1 << operand;
-    wire [31:0] wire_srl = din1 >> operand;
+	wire [31:0] wire_srl = din1 >> operand;//operand stores the values of no.of shifts.suppose the number of shifts were 4 then operand value will be 4,din1 shifted 4 times and the value will be stored in wire sr1
     wire [31:0] wire_sra = sign_din1 >>> operand;
 	
 	
-    assign dout = ( wire_add_en    ) ? wire_add32 
+	assign dout = ( wire_add_en    ) ? wire_add32 //if wire_add_en=1,do addition operation
 	:	          ( wire_sub_en    ) ? wire_sub32 
 	:	          ( wire_xor_en    ) ? wire_xor32 
 	:	          ( wire_or_en     ) ? wire_or32  
@@ -61,4 +61,4 @@ module ALU(
 	:	          ( control==8'h0e ) ? wire_sra
 	:	            32'd0;
 	
-endmodule
+endmodule //control signals for comparison operations are given seperately
