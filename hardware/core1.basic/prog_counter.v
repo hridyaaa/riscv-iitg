4a1b4e90f5b7f5bdd3e06ab5a957ca061487ee6c
hridyaaa <106798734+hridyaaa@users.noreply.github.com>
2022-06-05 13:54:25 +0530

    Update prog_counter.v

--- hardware/core1.basic/prog_counter.v
+++ hardware/core1.basic/prog_counter.v
@@ -21,17 +21,17 @@ module prog_counter(
 		output vinst_out
     );
 	
-	wire pc_update = pc_update1 | pc_update2 | pc_update3 | pc_update4 ;
+	wire pc_update = pc_update1 | pc_update2 | pc_update3 | pc_update4 ;//signal representing any of the pc update
 	
 	reg reg_pinst;
 	reg [31:0] reg_pc;  // current instruction address
 	
 	wire [31:0] wire_pc = pc_update ? pc_update_add
-	:                                  reg_pc + 32'd4 ;
+	:                                  reg_pc + 32'd4 ;//either pc goes to next instruction,or will be updated to a new physical address
 	
-	always@(posedge clk) if (reset) reg_pc <= 32'h000001fc ; else if (req) reg_pc <= wire_pc ;
+	always@(posedge clk) if (reset) reg_pc <= 32'h000001fc ; else if (req) reg_pc <= wire_pc ;//on reseting pc initialised to 01fc//address stored in pc
 	
-	always@(posedge clk) if (reset) reg_pinst <=  1'b0 ; else if (req) reg_pinst <= pc_update1 ;
+	always@(posedge clk) if (reset) reg_pinst <=  1'b0 ; else if (req) reg_pinst <= pc_update1 ;//instruction is stored in pc inst
 	
 //-------------------------------------------------------------- Output signals
 	
@@ -50,14 +50,14 @@ module prog_counter(
 		if      ( reset     ) buffer_full<=1'b0; 
 		else if ( pc_update ) buffer_full<=1'b0; 
 		else if ( buffer_en ) buffer_full<=1'b1; 
-		else if ( !pause    ) buffer_full<=1'b0;
+		else if ( !pause    ) buffer_full<=1'b0;// store address in buffer
 	end
 	
 	reg [31:0] buffer_inst; 
 	always@(posedge clk) begin
 		if      ( reset     ) buffer_inst<=32'd0; 
 		else if ( buffer_en ) buffer_inst<=inst; 
-		else if ( !pause    ) buffer_inst<=32'd0;
+		else if ( !pause    ) buffer_inst<=32'd0;//store corresponding instruction in buffer
 	end
 	
 //-------------------------------------------------------------- Output signals
