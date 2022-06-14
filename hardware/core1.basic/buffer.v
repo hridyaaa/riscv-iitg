55d31c1d01fb5770059a573aa5c984dfde1f0cb9
hridyaaa <106798734+hridyaaa@users.noreply.github.com>
2022-06-14 10:55:30 +0530

    Update buffer.v

--- hardware/core1.basic/buffer.v
+++ hardware/core1.basic/buffer.v
@@ -14,9 +14,9 @@ module buffer#(
 );
 
 	reg [WIDTH1-1:0] reg_din1 ; always@(posedge clk) if (reset) reg_din1 <= {WIDTH1{1'b0}} ; else if (!pause) reg_din1 <= din1 ;
-	reg [WIDTH2-1:0] reg_din2 ; always@(posedge clk) if (reset) reg_din2 <= {WIDTH2{1'b0}} ; else if (!pause) reg_din2 <= din2 ;
+	reg [WIDTH2-1:0] reg_din2 ; always@(posedge clk) if (reset) reg_din2 <= {WIDTH2{1'b0}} ; else if (!pause) reg_din2 <= din2 ;//if not in pause condition ,thenthe inputs to alu,din1 and din2 temp stored in reg_din1 and reg_din2 respectively
 	
 	assign dout1 = reg_din1 ;
-	assign dout2 = pause ? {WIDTH2{1'b0}} : reg_din2 ;
+	assign dout2 = pause ? {WIDTH2{1'b0}} : reg_din2 ;//output=dout1 and dout2 is coming from the buffer
 	
 endmodule
